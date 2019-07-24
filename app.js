8833305f6137c5c11ec973e556d92cee97d25f25
skyForge <1281alexdeo@gmail.com>
2019-07-24 16:10:21 +1100

    added new item from form and into data structure

--- app.js
+++ app.js
@@ -4,27 +4,60 @@ const vc_list = document
   .getElementById('wrapper-card')
   .getElementsByTagName('ul')[0];
 
+//VC_Controller
+const VC_Controller = (function() {
+  let VirtChanel = function(id, name, pri, min, max, exp) {
+    this.id = id;
+    this.name = name;
+    this.pri = pri;
+    this.min = min;
+    this.max = max;
+    this.exp = exp;
+  };
+  let data = {
+    availableBw: 10,
+    vc: []
+  };
+  //public methods
+  return {
+    addItem: function(name, pri, min, max, exp) {
+      //create new id
+      let ID = data.vc[data.vc.length - 1] + 1;
+      //create new item
+      let newItem = new VirtChanel(ID, name, pri, min, max, exp);
+      //push new item into data structure
+      data.vc.push(newItem);
+      //Return the new element
+
+      return newItem;
+    }
+  };
+})();
+
 Virtual_Chanels = [
   {
     name: 'P2P',
     priority: 1,
     min: null,
     max: null,
-    expedite: null
+    expedite: null,
+    calculatedBW: 0
   },
   {
     name: 'VOIP',
     priority: 2,
     min: null,
     max: null,
-    expedite: null
+    expedite: null,
+    calculatedBW: 0
   },
   {
     name: 'Streaming',
     priority: 3,
     min: null,
     max: null,
-    expedite: null
+    expedite: null,
+    calculatedBW: 0
   }
 ];
 
@@ -49,10 +82,18 @@ const UIcontroller = (function() {
 btn_add.addEventListener('click', () => {
   let inputs = UIcontroller.getInput();
   console.log(inputs);
+  let addedItem = VC_Controller.addItem(
+    inputs.name,
+    inputs.pri,
+    inputs.min,
+    inputs.max,
+    inputs.exp
+  );
+  console.log('added', addedItem);
 
   let li = document.createElement('li');
   li.classList = 'd-flex list-group-item vc-list-item  justify-content-between';
-  !inputs.name ? (li.textContent = none) : (li.textContent = `${inputs.name} `);
+  // !inputs.name ? (li.textContent = none) : (li.textContent = `${inputs.name} `);
 
   const i = document.createElement('i');
   i.innerHTML = `<span><i id="icon-trash" class="fas fa-trash"></i></span>`;
@@ -77,9 +118,11 @@ btn_add.addEventListener('click', () => {
   const bandwidth = (sum_of_pri, avail_bw) => {
     Virtual_Chanels.map(vc => {
       let result = (vc.priority / sum_of_pri) * avail_bw;
-      console.log(`${vc.name}: bandwidth: ${result.toFixed(2)}`);
+      vc.calculatedBW = result;
+      console.log(`${vc.name}: bandwidth: ${vc.calculatedBW.toFixed(2)}`);
     });
   };
 
   console.log(bandwidth(sum, Avail_BW));
+  // console.log(Virtual_Chanels);
 });
